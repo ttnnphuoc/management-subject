2e47464f5e5c0e8f3678d10ae4b8edf73c68baa0
ttnnphuoc <ttnnphuoc@gmail.com>
2020-07-15 00:54:11 +0700

    add source

---
+++ GoogleDriveRestAPI-v3-CreateFolder_Insert file_GetContainofFolder/GoogleDriveRestAPI-v3/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElearningSubject.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ChangeInfoAccount()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ GoogleDriveRestAPI-v3-CreateFolder_Insert file_GetContainofFolder/GoogleDriveRestAPI-v3/Models/GoogleDriveFilesRepository.cs
@@ -0,0 +1,200 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v2.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Web;
+
+namespace ElearningSubject_v3.Models
+{
+    public class GoogleDriveFilesRepository
+    {
+        public static string[] Scopes = { Google.Apis.Drive.v3.DriveService.Scope.Drive };
+        public static Google.Apis.Drive.v3.DriveService GetService_v3()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(@"D:\client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                String FolderPath = @"D:\";
+                String FilePath = Path.Combine(FolderPath, "DriveServiceCredentials.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
+            }
+
+            //Create Drive API service.
+            Google.Apis.Drive.v3.DriveService service = new Google.Apis.Drive.v3.DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveRestAPI-v3",
+            });
+
+            return service;
+        }
+        public static Google.Apis.Drive.v2.DriveService GetService_v2()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(@"D:\client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                String FolderPath = @"D:\";
+                String FilePath = Path.Combine(FolderPath, "DriveServiceCredentials.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
+            }
+
+            //Create Drive API service.
+            Google.Apis.Drive.v2.DriveService service = new Google.Apis.Drive.v2.DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveRestAPI-v2",
+            });
+
+            return service;
+        }
+        public static List<GoogleDriveFiles> GetContainsInFolder(String folderId)
+        {
+            List<string> ChildList = new List<string>();
+            Google.Apis.Drive.v2.DriveService ServiceV2 = GetService_v2();
+            ChildrenResource.ListRequest ChildrenIDsRequest = ServiceV2.Children.List(folderId);
+            do
+            {
+                ChildList children = ChildrenIDsRequest.Execute();
+
+                if (children.Items != null && children.Items.Count > 0)
+                {
+                    foreach (var file in children.Items)
+                    {
+                        ChildList.Add(file.Id);
+                    }
+                }
+                ChildrenIDsRequest.PageToken = children.NextPageToken;
+
+            } while (!String.IsNullOrEmpty(ChildrenIDsRequest.PageToken));
+ 
+            //Get All File List
+            List<GoogleDriveFiles> AllFileList = GetDriveFiles();
+            List<GoogleDriveFiles> Filter_FileList = new List<GoogleDriveFiles>();
+
+            foreach (string Id in ChildList)
+            {
+                Filter_FileList.Add(AllFileList.Where(x => x.Id == Id).FirstOrDefault());
+            }
+            return Filter_FileList;
+        }
+        public static void CreateFolder(string FolderName)
+        {
+            Google.Apis.Drive.v3.DriveService service = GetService_v3();
+
+            Google.Apis.Drive.v3.Data.File FileMetaData = new Google.Apis.Drive.v3.Data.File();
+            FileMetaData.Name = FolderName;
+            FileMetaData.MimeType = "application/vnd.google-apps.folder";
+
+            Google.Apis.Drive.v3.FilesResource.CreateRequest request;
+
+            request = service.Files.Create(FileMetaData);
+            request.Fields = "id";
+            var file = request.Execute();
+            //Console.WriteLine("Folder ID: " + file.Id);
+        }
+        public static void FileUploadInFolder(string folderId, HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                Google.Apis.Drive.v3.DriveService service = GetService_v3();
+
+                string path = Path.Combine(HttpContext.Current.Server.MapPath("~/GoogleDriveFiles"),
+                Path.GetFileName(file.FileName));
+                file.SaveAs(path);
+
+                var FileMetaData = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = Path.GetFileName(file.FileName),
+                    MimeType = MimeMapping.GetMimeMapping(path),
+                    Parents = new List<string>
+                    {
+                        folderId
+                    }
+                };
+
+                Google.Apis.Drive.v3.FilesResource.CreateMediaUpload request;
+                using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(FileMetaData, stream, FileMetaData.MimeType);
+                    request.Fields = "id";
+                    request.Upload();
+                }
+                var file1 = request.ResponseBody;
+            }
+        }
+        public static List<GoogleDriveFiles> GetDriveFiles()
+        {
+            Google.Apis.Drive.v3.DriveService service = GetService_v3();
+
+            // Define parameters of request.
+            Google.Apis.Drive.v3.FilesResource.ListRequest FileListRequest = service.Files.List();
+            FileListRequest.Fields = "nextPageToken, files(createdTime, id, name, size, version, trashed, parents)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = FileListRequest.Execute().Files;
+            List<GoogleDriveFiles> FileList = new List<GoogleDriveFiles>();
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    GoogleDriveFiles File = new GoogleDriveFiles
+                    {
+                        Id = file.Id,
+                        Name = file.Name,
+                        Size = file.Size,
+                        Version = file.Version,
+                        CreatedTime = file.CreatedTime,
+                        Parents = file.Parents
+                    };
+                    FileList.Add(File);
+                }
+            }
+            return FileList;
+        }
+        public static void FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                Google.Apis.Drive.v3.DriveService service = GetService_v3();
+
+                string path = Path.Combine(HttpContext.Current.Server.MapPath("~/GoogleDriveFiles"),
+                Path.GetFileName(file.FileName));
+                file.SaveAs(path);
+
+                var FileMetaData = new Google.Apis.Drive.v3.Data.File();
+                FileMetaData.Name = Path.GetFileName(file.FileName);
+                FileMetaData.MimeType = MimeMapping.GetMimeMapping(path);
+
+                Google.Apis.Drive.v3.FilesResource.CreateMediaUpload request;
+
+                using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(FileMetaData, stream, FileMetaData.MimeType);
+                    request.Fields = "id";
+                    request.Upload();
+                }
+            }
+        }
+
+        
+    }
+}
