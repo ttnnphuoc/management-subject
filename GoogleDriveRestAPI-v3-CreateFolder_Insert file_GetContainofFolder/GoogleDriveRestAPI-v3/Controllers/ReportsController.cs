1eeb8cac7d25cb860de808b1f97102a7451331ea
ttnnphuoc <ttnnphuoc@gmail.com>
2020-10-13 21:36:59 +0700

    group table report

--- GoogleDriveRestAPI-v3-CreateFolder_Insert file_GetContainofFolder/GoogleDriveRestAPI-v3/Controllers/ReportsController.cs
+++ GoogleDriveRestAPI-v3-CreateFolder_Insert file_GetContainofFolder/GoogleDriveRestAPI-v3/Controllers/ReportsController.cs
@@ -10,11 +10,19 @@ namespace ElearningSubject.Controllers
     public class ReportsController : Controller
     {
         // GET: Reports
+        Users user = new Users();
+        Departments dep = new Departments();
         public ActionResult Index()
         {
             if (CommonFunc.IsNotLogin(Session["UserLogin"] + ""))
                 return RedirectToAction("Login", "Accounts");
+            ViewBag.ListDepartment = dep.GetAll();
+            ViewBag.ListUser = user.GetAll();
             return View();
         }
+        //public JsonResult GetAllUser()
+        //{
+        //    return new JsonResult { Data = user.GetAll(), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        //}
     }
 }
